538f04c1b04a34ac5733c6e6f33464d73b5173a7
AyusH1912 <aayushsrivastava614@gmail.com>
2023-12-22 12:46:24 +0530

    starting java dsa sheet

---
+++ demo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Hey " + x + " times!");
+        sc.close();
+    }
+}
